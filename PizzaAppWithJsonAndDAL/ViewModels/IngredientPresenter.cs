cd1fc0fce3c428c9a4fae8358a84a9c2518d7ba6
Bjoern <bjoe6880@elev.techcollege.dk>
2022-04-22 08:24:39 +0200

    Added INotifyPropertyChanged to IngredientPresenter Class to make the toppinglist update when text is changed and nothing add/removed to the actual collection. Event set to trigger when menuText is set.

--- PizzaAppWithJsonAndDAL/ViewModels/IngredientPresenter.cs
+++ PizzaAppWithJsonAndDAL/ViewModels/IngredientPresenter.cs
@@ -1,20 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PizzaAppWithJsonAndDAL.ViewModels
 {
-    internal class IngredientPresenter
+    internal class IngredientPresenter : INotifyPropertyChanged
     {
-        public string menuText { get; set; }
+        private string _menuText;
+        public string menuText 
+        { 
+            get
+            {
+                return _menuText;
+            }
+            set
+            {
+                _menuText = value;
+                OnPropertyChanged(nameof(menuText));
+            }
+        }
         public int menuID { get; set; }
 
         public IngredientPresenter(int iId, string iText)
         {
             menuID = iId;
             menuText = iText;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string PropertyNavn)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyNavn));
+                //OnPropertyChanged("Beskrivelse");
+            }
         }
     }
 }
